66ceb89dbe9f9d71570ceb2bcb6915ae47c03115
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-03-20 21:45:55 -0300

    Adicionando tipos da classe Medico

--- MVC-hospitalar-backend/src/br/com/mvcHospitalar/model/Medico.java
+++ MVC-hospitalar-backend/src/br/com/mvcHospitalar/model/Medico.java
@@ -1,5 +1,75 @@
 package br.com.mvcHospitalar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Medico {
 
+	private int id;
+	private String nome;
+	private String especialidade;
+	private String crm;
+	private boolean isPlantao;
+	List<Paciente> pacientes = new ArrayList<>();
+	
+	public Medico(int id, String nome, String especialidade, String crm, boolean isPlantao, List<Paciente> pacientes) {
+		this.id = id;
+		this.nome = nome;
+		this.especialidade = especialidade;
+		this.crm = crm;
+		this.isPlantao = isPlantao;
+		this.pacientes = pacientes;
+	}
+
+	public Medico() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public String getCrm() {
+		return crm;
+	}
+
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+
+	public boolean isPlantao() {
+		return isPlantao;
+	}
+
+	public void setPlantao(boolean isPlantao) {
+		this.isPlantao = isPlantao;
+	}
+
+	public List<Paciente> getPacientes() {
+		return pacientes;
+	}
+
+	public void setPacientes(List<Paciente> pacientes) {
+		this.pacientes = pacientes;
+	}
+	
 }
