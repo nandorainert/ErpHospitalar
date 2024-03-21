adfad1871823d015516055c50a2204126336624a
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-03-20 22:01:06 -0300

    adicionando classe PacienteDAOImpl

---
+++ MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/PacienteDAOImpl.java
@@ -0,0 +1,50 @@
+package br.com.mvcHospitalar.DAO;
+
+import java.sql.Connection;
+import java.util.List;
+
+import br.com.mvcHospitalar.util.ConnectionFactory;
+
+public class PacienteDAOImpl implements GenericDAO {
+
+	private Connection conn;
+
+	public PacienteDAOImpl() throws Exception {
+		try {
+			this.conn = ConnectionFactory.getConnection();
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	@Override
+	public List<Object> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object listarPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean cadastrar(Object object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean alterar(Object object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void excluir(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
