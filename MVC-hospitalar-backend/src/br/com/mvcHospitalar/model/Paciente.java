c130dadbf71ec2b4fdf608a69a26fc5c5e8d4d99
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-03-20 21:40:50 -0300

    Adicionando tipos da classe Paciente

--- MVC-hospitalar-backend/src/br/com/mvcHospitalar/model/Paciente.java
+++ MVC-hospitalar-backend/src/br/com/mvcHospitalar/model/Paciente.java
@@ -1,5 +1,62 @@
 package br.com.mvcHospitalar.model;
 
 public class Paciente {
-
+	
+	private int id;
+	private String nome;
+	private String cpf;
+	private boolean isInternado;
+	private int idade;
+	private Medico medico;
+	
+	public Paciente(int id, String nome, String cpf, boolean isInternado, int idade, Medico medico) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.isInternado = isInternado;
+		this.idade = idade;
+		this.medico = medico;
+	}
+	
+	public Paciente () {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public boolean isInternado() {
+		return isInternado;
+	}
+	public void setInternado(boolean isInternado) {
+		this.isInternado = isInternado;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Medico getMedico() {
+		return medico;
+	}
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	
 }
