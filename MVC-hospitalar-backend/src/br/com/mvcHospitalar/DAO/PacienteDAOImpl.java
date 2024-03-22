7a3c2d2aa814bb4473a25838e4900e160a4468b4
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-03-22 18:48:13 -0300

    Função de listar para paciente.

--- MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/PacienteDAOImpl.java
+++ MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/PacienteDAOImpl.java
@@ -1,8 +1,13 @@
 package br.com.mvcHospitalar.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import br.com.mvcHospitalar.model.Paciente;
 import br.com.mvcHospitalar.util.ConnectionFactory;
 
 public class PacienteDAOImpl implements GenericDAO {
@@ -18,33 +23,58 @@ public class PacienteDAOImpl implements GenericDAO {
 	}
 	
 	@Override
-	public List<Object> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Object> listarTodos() {	
+		
+		List<Object> lista = new ArrayList<Object>();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM paciente";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				Paciente paciente = new Paciente();
+				paciente.setId(rs.getInt("id"));
+				paciente.setNome(rs.getString("Nome"));
+				paciente.setCpf(rs.getString("CPF"));
+				paciente.setIdade(rs.getInt("Idade"));
+				lista.add(paciente);
+			}
+		} catch (SQLException ex) {
+			System.out.println("Problemas na DAO ao listar Paciente! " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, rs);
+			} catch (Exception e) {
+				System.out.println("Problemas na DAO ao fechar conexão! " + e.getMessage());
+			}
+		}
+		return lista;
 	}
 
 	@Override
 	public Object listarPorId(int id) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public boolean cadastrar(Object object) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public boolean alterar(Object object) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public void excluir(int id) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
