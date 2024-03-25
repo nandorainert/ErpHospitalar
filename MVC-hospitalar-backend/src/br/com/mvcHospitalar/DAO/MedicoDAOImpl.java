c25a8db500801c1cf46f5d0b04c76763df8b9bf7
Ferdinando Rainert <ferdinandorainert24@gmail.com>
2024-03-25 18:35:27 -0300

    adicionando metodos padrao a classe medicoDAOImpl

--- MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/MedicoDAOImpl.java
+++ MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/MedicoDAOImpl.java
@@ -1,14 +1,18 @@
 package br.com.mvcHospitalar.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-
+import br.com.mvcHospitalar.model.Medico;
 import br.com.mvcHospitalar.util.ConnectionFactory;
 
 public class MedicoDAOImpl implements GenericDAO {
 
 	private Connection conn;
-	
+
 	public MedicoDAOImpl() throws Exception {
 		try {
 			this.conn = ConnectionFactory.getConnection();
@@ -16,34 +20,139 @@ public class MedicoDAOImpl implements GenericDAO {
 			throw new Exception(e.getMessage());
 		}
 	}
+
 	@Override
 	public List<Object> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Object> lista = new ArrayList<Object>();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM medico";
+		try {
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				Medico medico = new Medico();
+				medico.setId(rs.getInt("id"));
+				medico.setNome(rs.getString("Nome"));
+				lista.add(medico);
+			}
+		} catch (SQLException ex) {
+			System.out.println("Problemas na DAO ao listar Medico! " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, rs);
+			} catch (Exception e) {
+				System.out.println("Problemas na DAO ao fechar conexão! " + e.getMessage());
+			}
+		}
+
+		return lista;
 	}
 
 	@Override
 	public Object listarPorId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Medico medico = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM medico WHERE id = (?)";
+
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				medico = new Medico();
+				medico.setId(rs.getInt("id"));
+				medico.setNome(rs.getString("nome"));
+			}
+
+		} catch (SQLException ex) {
+			System.out.println("Problemas na DAO ao listar Medico por id! " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, rs);
+			} catch (Exception e) {
+				System.out.println("Problemas na DAO ao fechar conexão! " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+
+		return medico;
 	}
 
 	@Override
 	public boolean cadastrar(Object object) {
-		// TODO Auto-generated method stub
-		return false;
+		Medico medico = (Medico) object;
+		PreparedStatement stmt = null;
+		String sql = "INSERT INTO produto (nome) VALUES (?)";
+
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, medico.getNome());
+			stmt.execute();
+			return true;
+		} catch (SQLException ex) {
+			System.out.println("Problemas na DAO ao cadastrar medico " + ex.getMessage());
+			ex.printStackTrace();
+			return false;
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, null);
+			} catch (Exception e) {
+				System.out.println("Problemas na DAO ao fechar conexão! " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public boolean alterar(Object object) {
-		// TODO Auto-generated method stub
-		return false;
+		Medico medico = (Medico) object;
+		PreparedStatement stmt = null;
+		String sql = "UPDATE produto SET descricao = ? WHERE id = ?";
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, medico.getNome());
+			stmt.setInt(2, medico.getId());
+			stmt.execute();
+			return true;
+		} catch (Exception e) {
+			System.out.println("Problemas na DAO ao cadastrar medico " + e.getMessage());
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, null);
+			} catch (Exception e) {
+				System.out.println("Problemas na DAO ao fechar conexão! " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public void excluir(int id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement stmt = null;
+		String sql = "DELETE FROM medico WHERE id = (?)";
+
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			stmt.execute();
+		} catch (Exception e) {
+			System.out.println("Problemas na DAO ao excluir medico! " + e.getMessage());
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, null);
+			} catch (Exception e) {
+				System.out.println("Problemas na DAO ao fechar conexão! " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/PacienteDAOImpl.java
+++ MVC-hospitalar-backend/src/br/com/mvcHospitalar/DAO/PacienteDAOImpl.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import br.com.mvcHospitalar.model.Paciente;
 import br.com.mvcHospitalar.util.ConnectionFactory;
 
